9253bb3b8368516be28c35b29cfccd4e914f4471
sasha-ajin <sashaajin@gmail.com>
2024-12-09 00:20:16 +0200

    add tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
     </head>
     <body class="antialiased">
     <div id="root"></div>
