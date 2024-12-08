03e2da489613e0c54e9cb29a35da4c4d9d44ee7f
sasha-ajin <sashaajin@gmail.com>
2024-12-09 01:01:09 +0200

    Updated .gitignore and removed cached files

--- resources/assets/index.tsx
+++ resources/assets/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Updated import
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./output.css"
 
 if (document.getElementById("root")) {
     const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement); // Updated usage
