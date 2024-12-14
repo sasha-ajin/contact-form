4d2ed096b6532b21d1e7301081786f9ccbe5075d
sasha-ajin <sashaajin@gmail.com>
2024-12-14 22:44:42 +0200

    remove convertObjectToFormData by using json instead of form-data

--- resources/assets/modules/contact/service.ts
+++ resources/assets/modules/contact/service.ts
@@ -1,17 +1,14 @@
-import {convertObjectToFormData} from "@/utils/convertObjectToFormData";
 import {ContactResponse, ContactValues} from "@/modules/contact/models";
 import axios from 'axios';
 
 const STORE_CONTACT_ENDPOINT = '/api/contact'
 
 export async function storeContact(values: ContactValues) {
-    const formData = convertObjectToFormData(values);
-
     const response = await axios.post<ContactResponse>(
         STORE_CONTACT_ENDPOINT,
-        formData,
+        values,
         {
-            headers: {'Content-Type': 'multipart/form-data'},
+            headers: {'Content-Type': 'application/json'},
         }
     );
 
--- resources/assets/utils/convertObjectToFormData.ts
+++
@@ -1,9 +0,0 @@
-export function convertObjectToFormData<T extends Record<string, any>>(value: T): FormData {
-    const formData = new FormData()
-
-    Object.keys(value).forEach((propertyName) => {
-        formData.append(propertyName, value[propertyName])
-    });
-
-    return formData
-}
