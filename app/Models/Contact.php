fb0bdc122b689be3f85168cedef6333fc6a47603
sasha-ajin <sashaajin@gmail.com>
2024-12-14 15:54:07 +0200

    add phpDoc to Contact.php

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,7 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $phone
+ * @property string $message
+ */
 class Contact extends Model
 {
-    public $timestamps = false;
 }
