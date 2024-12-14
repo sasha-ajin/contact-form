1f258ab50523cad1583e371f3e9ad188043f7348
sasha-ajin <sashaajin@gmail.com>
2024-12-14 18:06:59 +0200

    rename SEND_CONTACTS_ENDPOINT constant to SEND_CONTACT_ENDPOINT

--- resources/assets/modules/contact/service.ts
+++ resources/assets/modules/contact/service.ts
@@ -2,13 +2,13 @@ import {convertObjectToFormData} from "@/utils/convertObjectToFormData";
 import {ContactResponse, ContactValues} from "@/modules/contact/models";
 import axios from 'axios';
 
-const SEND_CONTACTS_ENDPOINT = '/api/contact'
+const SEND_CONTACT_ENDPOINT = '/api/contact'
 
 export async function sendContact(values: ContactValues) {
     const formData = convertObjectToFormData(values);
 
     const response = await axios.post<ContactResponse>(
-        SEND_CONTACTS_ENDPOINT,
+        SEND_CONTACT_ENDPOINT,
         formData,
         {
             headers: {'Content-Type': 'multipart/form-data'},
